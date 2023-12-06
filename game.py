53a1c4cdd3d2047fb439488f554a037d9fc0e4ec
kh3dron <tristansaldanha@gmail.com>
2023-12-05 23:33:30 -0800

    groups & liberties

--- game.py
+++ game.py
@@ -13,6 +13,9 @@ class Go:
         self.captured_black = 0
         self.captured_white = 0
         
+    def __str__(self):
+        return str(self.board)
+        
     def is_empty(self, i, j):
         return self.board[i, j] == 0
     
@@ -33,6 +36,67 @@ class Go:
             self.player = 1 if self.player == 2 else 2
         
         return
+        
+    def get_groups(self, color):
+        # Returns a tuple of two matrices, one for the groups and one for the liberties. 
+        # The group matrix contains a unique number for each group of stones of the same color.
+        # The liberties matrix contains a count of liberties for each group.
+
+        groups = np.zeros((self.board_size, self.board_size), dtype=int)
+        visited = set()
+        group_number = 1
+
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if self.board[i, j] == color and (i, j) not in visited:
+                    group, liberty_set = self._dfs(color, i, j, visited)
+
+                    # Assign group number to the group cells
+                    for x, y in group:
+                        groups[x, y] = group_number
+
+                    group_number += 1
+
+        return groups
+
+    def _dfs(self, color, start_x, start_y, visited):
+        group = set()
+        liberty_set = set()
+        stack = [(start_x, start_y)]
+
+        while stack:
+            x, y = stack.pop()
+            if (x, y) not in visited:
+                visited.add((x, y))
+                group.add((x, y))
+
+                # Check and add liberties
+                for nx, ny in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+                    if 0 <= nx < self.board_size and 0 <= ny < self.board_size:
+                        if self.board[nx, ny] == 0:
+                            liberty_set.add((nx, ny))
+
+                # Check and add neighboring stones of the same color
+                for nx, ny in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+                    if 0 <= nx < self.board_size and 0 <= ny < self.board_size:
+                        if self.board[nx, ny] == color:
+                            stack.append((nx, ny))
+
+        return group, liberty_set
+
+    def get_liberties(self, color, group):
+        # Returns a 1 for each cell that is a liberty of the group
+        liberties = np.zeros((self.board_size, self.board_size), dtype=int)
+
+        for x, y in group:
+            for nx, ny in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+                if 0 <= nx < self.board_size and 0 <= ny < self.board_size:
+                    if self.board[nx, ny] == 0:
+                        liberties[nx, ny] = 1
+
+        return liberties
+
+        
     
     def get_winner(self):
         if self.captured_black > self.captured_white:
--- tests.py
+++ tests.py
@@ -1,20 +1,21 @@
+import numpy as np
 import unittest
 from game import Go
 
-class TestGoGame(unittest.TestCase):
 
+class TestGoGame(unittest.TestCase):
     def test_is_empty(self):
         self.game = Go(9)
         self.assertTrue(self.game.is_empty(0, 0))
         self.game.board[0, 0] = 1
         self.assertFalse(self.game.is_empty(0, 0))
-        
+
     def test_place_stone(self):
         self.game = Go(9)
         self.game.place_stone(0, 0)
         self.assertEqual(self.game.board[0, 0], 1)
         self.assertEqual(len(self.game.moves), 1)
-        
+
     def test_place_stone_occupied(self):
         self.game = Go(9)
         self.game.place_stone(0, 0)
@@ -23,18 +24,69 @@ class TestGoGame(unittest.TestCase):
 
     def test_pass_turn(self):
         self.game = Go(9)
-        self.game.place_stone(0, 0) #black
-        self.game.pass_turn() #white
+        self.game.place_stone(0, 0)  # black
+        self.game.pass_turn()  # white
         self.assertEqual(self.game.player, 1)
         self.assertEqual(self.game.moves[-1], (-1, -1))
 
     def test_double_pass(self):
         self.game = Go(9)
-        
         self.game.pass_turn()
         self.game.pass_turn()
         self.assertTrue(self.game.ended)
-        
 
-if __name__ == '__main__':
+    def test_get_groups_small_groups(self):
+        self.game = Go(9)
+        self.game.board[0, 0] = 1
+        self.game.board[0, 4] = 1
+        self.game.board[6, 6] = 1
+        self.game.board[6, 7] = 1
+        groups = self.game.get_groups(1)
+
+        expected_groups = np.array(
+            [
+                [1, 0, 0, 0, 2, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 3, 3, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        )
+
+        self.assertTrue((groups == expected_groups).all())
+
+    def test_get_groups_close_groups(self):
+        self.game = Go(9)
+        self.game.board[3, 5] = 1
+        self.game.board[5, 5] = 1
+        self.game.board[5, 6] = 1
+        self.game.board[6, 6] = 1
+        self.game.board[6, 7] = 1
+        groups = self.game.get_groups(1)
+
+        print(groups)
+
+        expected_groups = np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 2, 2, 0, 0],
+                [0, 0, 0, 0, 0, 0, 2, 2, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        )
+
+
+        self.assertTrue((groups == expected_groups).all())
+
+
+if __name__ == "__main__":
     unittest.main()
